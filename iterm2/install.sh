055cd674c91b5e9240b97c3477b183f0ee883347
sjakos <sjakos@users.noreply.github.com>
2018-10-25 07:00:30 -0500

    feat(updates): update iterm, zsh, os-defaults, and vscode

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -13,7 +13,7 @@ if test "$(uname)" = "Darwin"; then
 
   # Thanks! 👍 => http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
   # Specify the preferences directory
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
   # Tell iTerm2 to use the custom preferences in the directory
   defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 fi
