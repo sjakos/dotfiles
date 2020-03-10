2ffe62f779160784f6ef8f8ec7cbb35ae1c1de7d
sjakos <sjakos@users.noreply.github.com>
2020-03-10 09:08:06 -0500

    feat(updates): general update from Aug 2019

--- nvm/install.sh
+++ nvm/install.sh
@@ -7,7 +7,7 @@
 if [ ! -f $NVM_DIR/nvm.sh ];
 then
   echo "  Installing nvm for you."
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
   NVM_DIR="$HOME/.nvm" && . "$NVM_DIR/nvm.sh"
   ln -s "$DOTFILES/nvm/default-packages" "$NVM_DIR/default-packages"
   nvm install --lts
